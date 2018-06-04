1cb1c2b56ccca23d37b87d26e9b1ba2fe1c1c6f9
Yomguithereal <guillaumeplique@gmail.com>
2018-06-04 14:52:10 +0200

    Proper bitset

--- phylactery/bitset.py
+++ phylactery/bitset.py
@@ -4,7 +4,82 @@
 #
 # A simple Python BitSet implementation relying on Numpy.
 #
+import math
+import numpy as np
+
+MODULO = 0x0000001f
 
 
 class BitSet(object):
-    pass
+    """
+    The BitSet class.
+
+    Args:
+        capacity (number): Capacity of the bitset.
+
+    """
+
+    def __init__(self, capacity):
+        true_capacity = math.ceil(capacity / 32)
+
+        # Properties
+        self.capacity = capacity
+        self.__integers = np.zeros(true_capacity, dtype=np.int32)
+
+    def __len__(self):
+        return self.capacity
+
+    def get(self, index):
+
+        if index >= self.capacity:
+            raise IndexError
+
+        byte = index >> 5
+        pos = index & MODULO
+
+        return (self.__integers[byte] >> pos) & 1
+
+    def has(self, index):
+        return self.get(index) == 1
+
+    def test(self, index):
+        return self.has(index)
+
+    def set(self, index, value=1):
+        if index >= self.capacity:
+            raise IndexError
+
+        byte = index >> 5
+        pos = index & MODULO
+
+        if not value:
+            self.__integers[byte] &= ~(1 << pos)
+        else:
+            self.__integers[byte] |= (1 << pos)
+
+    def reset(self, index):
+        if index >= self.capacity:
+            raise IndexError
+
+        byte = index >> 5
+        pos = index & MODULO
+
+        self.__integers[byte] &= ~(1 << pos)
+
+    def add(self, index):
+        return self.set(index)
+
+    def __getitem__(self, index):
+        return self.get(index)
+
+    def __contains__(self, index):
+        return self.has(index)
+
+    def __setitem__(self, index, value):
+        return self.set(index, value)
+
+    def __delitem__(self, index):
+        return self.reset(index)
+
+    def __repr__(self):
+        return '<%s capacity=%i>' % (self.__class__.__name__, self.capacity)
--- test/bitset_test.py
+++ test/bitset_test.py
@@ -1,9 +1,53 @@
 # =============================================================================
 # Phylactery BitSet Unit Tests
 # =============================================================================
+import pytest
 from phylactery import BitSet
 
 
 class TestBitSet(object):
     def test_basics(self):
-        assert True
+        bitset = BitSet(4)
+
+        assert bitset.capacity == 4
+        assert len(bitset) == 4
+
+        assert bitset.get(2) == 0
+        assert bitset.get(1) == 0
+
+        with pytest.raises(IndexError):
+            bitset.get(34)
+
+        assert bitset[0] == 0
+        assert bitset[1] == 0
+
+        bitset.set(0)
+        bitset.set(1, True)
+        bitset.set(3, False)
+
+        assert bitset[0] == 1
+        assert bitset[1] == 1
+        assert bitset[3] == 0
+
+        assert bitset.test(0)
+        assert bitset.has(1)
+        assert 1 in bitset
+
+        bitset[2] = True
+
+        assert 2 in bitset
+
+        bitset.reset(2)
+
+        assert 2 not in bitset
+
+        del bitset[0]
+
+        assert bitset.get(0) == 0
+
+        with pytest.raises(IndexError):
+            bitset.add(5)
+
+        bitset.add(0)
+
+        assert 0 in bitset
