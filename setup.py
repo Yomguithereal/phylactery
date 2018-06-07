c08879932185d5329637be2e7c2a05db1ae224b3
Yomguithereal <guillaumeplique@gmail.com>
2018-06-07 16:12:46 +0200

    0.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('./README.md', 'r') as f:
     long_description = f.read()
 
 setup(name='phylactery',
-      version='0.0.4',
+      version='0.1.0',
       description='Curated collection of data structures for Python.',
       long_description=long_description,
       long_description_content_type='text/markdown',
